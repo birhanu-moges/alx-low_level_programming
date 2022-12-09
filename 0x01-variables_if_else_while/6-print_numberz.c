3645101919ac2adf349d53dd101c12aa281768ab
birhanu-moges <birhanu18@gmail.com>
2022-12-09 11:14:50 -0800

    task 6 c programm

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry function for c program
+ * Discription: print all letters
+ * Return:  (0) sucess
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
