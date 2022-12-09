8b65913212725fc643ecc927a569d039d11b1893
birhanu-moges <birhanu18@gmail.com>
2022-12-09 11:26:29 -0800

    task 8 c programm

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry function for c program
+ * Discription: print all letters
+ * Return:  (0) sucess
+ */
+int main(void)
+{
+	int a;
+	char i;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar((a % 10) + '0');
+	}
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
