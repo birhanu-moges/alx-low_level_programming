51836682147e457d41edcbc3619cf95fd59ee228
birhanu-moges <birhanu18@gmail.com>
2022-12-09 11:31:25 -0800

    task 9 c program

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry function for c program
+ * Discription: print all letters
+ * Return:  (0) sucess
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar((a % 10) + '0');
+		if (a < 9)
+			putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
