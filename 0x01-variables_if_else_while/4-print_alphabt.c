6fc7abe9cd9f22d6aa56d45ddfb32c3ee57e9d11
birhanu-moges <birhanu18@gmail.com>
2022-12-09 10:54:59 -0800

    task 4 c programm

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry function for c program
+ * Discription: print all letters
+ * Return:  (0) sucess
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+		putchar('\n');
+	}
+	return (0);
+}
