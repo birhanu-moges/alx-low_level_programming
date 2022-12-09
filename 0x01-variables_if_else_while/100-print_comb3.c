df058c2bf9982f0dfff601c0946672a95114a291
birhanu-moges <birhanu18@gmail.com>
2022-12-09 11:52:54 -0800

    task 10 c program

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry function for c program
+ * Discription: print all letters
+ * Return:  (0) sucess
+ */
+int main(void)
+{
+	int a;
+	int i;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (i = a + 1; i <= 9; i++)
+		{
+			putchar((a % 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
