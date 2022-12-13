11c9ab64a330fde313d12fef2d9ba9a16ca32425
birhanu-moges <birhanu18@gmail.com>
2022-12-13 10:44:05 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - entry point
+ * @n1: The number to be added
+ * @n2: The number to be added
+ * Discription: add a number
+ * Return: (0) success
+ */
+
+int add(int n1, int n2)
+{
+	retun (n1 + n2); 
+}
