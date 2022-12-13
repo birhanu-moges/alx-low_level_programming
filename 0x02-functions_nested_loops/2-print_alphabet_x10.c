827a36b5477b5ac2893bb13ca10df4bbeceb90bf
birhanu-moges <birhanu18@gmail.com>
2022-12-13 05:55:24 -0800

    task 2 c  function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ * Discription: printing lower case alphabets
+ * Return: (0) success
+ */
+
+void print_alphabet_x10(void)
+{
+	char a;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+	}
+	_putchar('\n');
+}
