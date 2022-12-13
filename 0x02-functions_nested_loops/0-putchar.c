6e9025eeb87692560604026b7bae8457d3ec1791
birhanu-moges <birhanu18@gmail.com>
2022-12-13 03:44:26 -0800

    task 0 c programm

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main - entry point 
+ * Discription: printing string
+ * Return: (0) success
+ */
+
+int main(void)
+{
+	printf("_putchar\");
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+C functions poject folder
