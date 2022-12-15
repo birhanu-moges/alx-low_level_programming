6da17659267785451bbf8ab75d4ff2de9d4d708d
birhanu-moges <birhanu18@gmail.com>
2022-12-15 05:38:39 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul- multiply two values
+ * Description: multiply
+ * @a: first value
+ * @b: second value
+ * Return: (product)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
