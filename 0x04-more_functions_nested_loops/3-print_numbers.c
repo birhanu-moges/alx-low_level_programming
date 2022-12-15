98a67dc61c135902492d5bc260b70fd151237924
birhanu-moges <birhanu18@gmail.com>
2022-12-15 05:45:53 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - multiply two values
+ * Description: print loop value
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
