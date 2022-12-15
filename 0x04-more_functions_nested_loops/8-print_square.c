d4c612b5c3cbec461e7bb12fabcd0a0a6dab61aa
birhanu-moges <birhanu18@gmail.com>
2022-12-15 09:22:22 -0800

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - checkes digit vialue
+ * Description: print square
+ * @size: input to determine  square size
+ * Return: (void)
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+	if (n <= 0)
+		_putchar('\n');
+}
