4c709b0a7d06a231a6b9badc70cd624a05b41364
birhanu-moges <birhanu18@gmail.com>
2022-12-15 09:40:22 -0800

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: fizz-buzz test
+ *
+ * Return: (1) success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i != 99)
+		printf(" ");
+	}
+
+		return (0);
+}
