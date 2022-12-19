b3e5f7b9dd06a9c8818a6afe1a4a76c811cce5a1
birhanu-moges <birhanu18@gmail.com>
2022-12-19 09:25:04 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_rev - printing in reverse order given string
+  * @s: given string
+  *
+  * Return: void
+  */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
