0fa8f4750bc4dd468a81f693108f3abb971e3e6c
birhanu-moges <birhanu18@gmail.com>
2022-12-19 06:29:15 -0800

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * swap_int - Updates the value it points to 98
+  * @a: pointer to a
+  * @b: pointer to b
+  *
+  * Return: void
+  */
+void swap_int(int *a, int *b)
+{
+	int *tmp;
+
+	*a = *b;
+	*b = tmp;
+
+}
