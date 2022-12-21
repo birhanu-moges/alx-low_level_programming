95686fb2330f596332675f09ce6528dfda57c7aa
birhanu-moges <birhanu18@gmail.com>
2022-12-21 10:53:50 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,15 +15,14 @@ char *_strcat(char *dest, char *src)
 		dest_length++;
 	while (src[src_length] != '\0')
 		src_length++;
-	src_length--;
 	int start_index = 0;
 
-	while (start_index <= src_length)
+	while (start_index < src_length)
 	{
-		dest[dest_length++] = src[i];
-		i++;
+		dest[dest_length++] = src[start_index++];
 	}
-	dest[dest_length] = '\0';
+	if (src_length > 0)
+		dest[dest_length] = '\0';
 	_putchar('\n');
 	return (*dest);
 }
