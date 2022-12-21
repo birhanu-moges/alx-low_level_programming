371ffce0973a51a26782f9a27e3d8f1474681946
birhanu-moges <birhanu18@gmail.com>
2022-12-21 13:05:01 -0800

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,13 +12,8 @@ char *string_toupper(char *str)
 
 	while (str[i])
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-		{
+		if (str[i] >= 'a' || str[i] <= 'z')
 			str[i] = str[i] - ' ';
-		}
-		i++;
 	}
-
 	return (str);
-}
 }
