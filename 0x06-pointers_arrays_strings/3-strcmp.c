f5835dfccbf5699d57a92646bf858fa5fc5d1176
birhanu-moges <birhanu18@gmail.com>
2022-12-21 12:14:51 -0800

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,13 +17,10 @@ int _strcmp(char *s1, char *s2)
 			i++;
 			continue;
 		}
-		else if (s1[i] > s2[i])
-			return (s1[i] - s2[i]);
-		else
-			return (s2[i] - s1[-]);
+		return (s1[i] - s2[i]);
 	}
 	if (s1[i] != '\0')
 		return (s1[i]);
-	return (s2[i]);
+	return (-1 * s2[i]);
 
 }
