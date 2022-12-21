f5cb01ae70e319aa79425e85e41aa707efd55d17
birhanu-moges <birhanu18@gmail.com>
2022-12-21 12:53:15 -0800

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * string_toupper - change string to upper
+  * @str: given String
+  *
+  * Return: uppercase string
+  */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ' ';
+		}
+		i++;
+	}
+
+	return (str);
+}
+}
