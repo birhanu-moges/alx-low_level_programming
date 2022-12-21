2b9ae249ff64010e289751b52e142a3df44ff39d
birhanu-moges <birhanu18@gmail.com>
2022-12-21 11:23:26 -0800

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * _strncat - concatenates two strings
+  * @dest: destination String
+  * @src: source String
+  * @n: total characters to concatinate
+  * Return: concatinated String
+  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_length = 0;
+	int src_length = 0;
+
+	while (dest[dest_length] != '\0')
+		dest_length++;
+
+	while (src_length < n)
+	{
+		dest[dest_length++] = src[src_length++];
+	}
+	if (src_length > 0)
+		dest[dest_length] = '\0';
+	return (dest);
+}
