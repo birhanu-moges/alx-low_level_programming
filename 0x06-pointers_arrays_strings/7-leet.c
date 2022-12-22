62591f9fecf3ddba0a1d942372f2c704517efd3c
birhanu-moges <birhanu18@gmail.com>
2022-12-21 23:04:43 -0800

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,8 +10,8 @@ char *leet(char *str)
 {
 	int i = 0;
 	int j = 0;
-	char letters[] = "aeotl";
-	char decode[] = "43071";
+	char letters[] = {'a', 'e', 'o', 't', 'l', '\0'};
+	char decode[] = {'4', '3', '0', '7', '1'};
 
 	while (str[i])
 	{
