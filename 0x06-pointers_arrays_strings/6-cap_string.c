ed64154ab959ab27e0ab96e0defd383d933ccb34
birhanu-moges <birhanu18@gmail.com>
2022-12-21 22:28:57 -0800

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,13 @@ char *cap_string(char *str)
 	while (str[i])
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-			if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\n')
+			if (i == 0 || (str[i - 1] == ' ' || str[i - 1] == '\t'
+			|| str[i - 1] == '\n' || str[i - 1] == ','
+			|| str[i - 1] == ';' || str[i - 1] == '.'
+			|| str[i - 1] == '!' || str[i - 1] == '?'
+			|| str[i - 1] == '"' || str[i - 1] == '('
+			|| str[i - 1] == ')' || str[i - 1] == '}'
+			|| str[i - 1] == '{'))
 				str[i] = str[i] - ' ';
 	i++;
 	}
