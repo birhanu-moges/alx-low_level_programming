4cbf0f29ee3203e455077c82c7cee3ea9ef56405
birhanu-moges <birhanu18@gmail.com>
2023-01-02 03:32:14 -0800

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _strchr - find first occurance of a character
+  * @s: source string
+  * @c: character to check
+  * Return: return pointer to the first occurance
+  */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (s[i]);
+		i++;
+	}
+	return (NULL);
+}
