b66e188d18a6d0109854ea37246ecb06ff573ce4
birhanu-moges <birhanu18@gmail.com>
2023-01-02 08:48:24 -0800

    task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_chessboard - return muching string from source
+  * @a: chess array
+  * Return: void
+  */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j = 0;
+
+	while (a)
+	{
+		for (i = 0; i < 7; i++)
+		{
+			_putchar(a[i][j]);
+		}
+		j++;
+		_putchar('\n');
+	}
+}
