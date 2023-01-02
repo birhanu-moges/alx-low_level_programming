186f6110575e9d72383a87f857bbd0a0bd35aff2
birhanu-moges <birhanu18@gmail.com>
2023-01-02 08:54:44 -0800

    task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,9 +9,9 @@ void print_chessboard(char (*a)[8])
 {
 	int i = 0, j = 0;
 
-	while (a)
+	for (j = 0; j < 8; j++)
 	{
-		for (i = 0; i < 7; i++)
+		for (i = 0; i < 8; i++)
 		{
 			_putchar(a[j][i]);
 		}
