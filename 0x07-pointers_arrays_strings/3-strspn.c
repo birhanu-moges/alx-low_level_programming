9e6590e054229b88d3c8bdcedee5f5e81b94d791
birhanu-moges <birhanu18@gmail.com>
2023-01-02 05:31:16 -0800

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,12 +11,15 @@ unsigned int _strspn(char *s, char *accept)
 	int i = 0, j = 0;
 	unsigned int bytes = 0;
 
-	for (i = 0; s[i] >= '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		while (accept[j])
 		{
 			if (s[i] == accept[j])
+			{
 				bytes++;
+				break;
+			}
 			j++;
 		}
 		j = 0;
