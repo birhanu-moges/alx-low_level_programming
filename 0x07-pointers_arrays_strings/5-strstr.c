18b0afc21d3e4bf739008062e0219098e1dcb316
birhanu-moges <birhanu18@gmail.com>
2023-01-02 06:09:52 -0800

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * _strstr - return muching string from source
+  * @haystack: source string
+  * @needle: characters to check
+  * Return: return matching character starting
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		while (needle[j])
+		{
+			if (s[i] != accept[j])
+			{
+				break;
+			}
+			j++;
+		}
+		if (needle[j] == '\0')
+			return (haystack + i - j);
+		j = 0;
+	}
+	return ('\0');
+}
