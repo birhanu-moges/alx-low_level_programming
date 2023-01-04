ac155df00d038365e7cd0ecccce08d7fb48ad0e9
birhanu-moges <birhanu18@gmail.com>
2023-01-03 23:54:55 -0800

    task 3

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,9 @@
   */
 int _strlen_recursion(char *s)
 {
-	int length = 0;
-
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		return (length);
+		return (1 + _strlen_recursion(s + 1));
 	}
-	length++;
-	_strlen_recursion(s + 1);
+	return (0);
 }
