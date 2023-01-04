20f6690596a5b5e03c02110e48c9dafa4beaeea9
birhanu-moges <birhanu18@gmail.com>
2023-01-03 23:35:33 -0800

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _print_rev_recursion - print string in reverse
+  * @s: string to print in reverse
+  *
+  * Return: void
+  */
+void _print_rev_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_puts_recursion(s + 1);
+	_putchar(*s);
+}
