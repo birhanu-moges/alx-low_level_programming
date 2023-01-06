b9c4f6febe798ca6275a245bcbe0b1d2ca3f1ac0
birhanu-moges <birhanu18@gmail.com>
2023-01-06 02:17:15 -0800

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,11 +11,15 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
+	int a, b;
+
+	if (argc != 3)
 	{
 		printf("Error\n");
-		return (-1);
+		return (1);
 	}
-	printf("%d\n", (atoi(*argv) * atoi((*(argv + 1))));
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	printf("%d\n", (a * b));
 	return (0);
 }
