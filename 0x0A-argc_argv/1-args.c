2f771efd28517fe2405d71445e7a1f9ee5e31912
birhanu-moges <birhanu18@gmail.com>
2023-01-06 01:53:52 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: total command line arguments
+ * @argv: string arrays in command line
+ *
+ * Return: (0) on success
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%d\n", argc - 1);
+	return (0);
+}
