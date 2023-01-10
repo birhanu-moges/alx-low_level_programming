dcf5b01d96b80e9e6dd5a5a728bc3cca0a5d27df
birhanu-moges <birhanu18@gmail.com>
2023-01-10 00:55:13 -0800

    task 3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,7 @@ char *str_concat(char *s1, char *s2)
 	unsigned int l1 = 0, l2 = 0, index = 0;
 	char *ptr;
 
-	while (s1[l1] != '\0'')
+	while (s1[l1] != '\0')
 		l1++;
 	while (s2[l2] != '\0')
 		l2++;
