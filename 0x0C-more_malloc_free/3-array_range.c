92ee701979d73b643ea25fed164273fb40c434ff
birhanu-moges <birhanu18@gmail.com>
2023-01-12 09:39:34 -0800

    task 4

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	ptr = malloc(max - min + 1);
+	ptr = malloc(sizeof(int) * (max - min + 1));
 	if (ptr == NULL)
 		return (NULL);
 	for (; min <= max; min++)
