fe780b9ebc216e2e71f25c387e92b5a60b2f79e5
birhanu-moges <birhanu18@gmail.com>
2023-01-16 05:25:46 -0800

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
  *
  * Description: dog object with its name, age and owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
