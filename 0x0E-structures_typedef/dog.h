b4195ea98d9add17e59373bd710bb80e551c6077
birhanu-moges <birhanu18@gmail.com>
2023-01-16 03:52:27 -0800

    task 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - elements of dog object
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ *
+ * Description: dog object with its name, age and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
