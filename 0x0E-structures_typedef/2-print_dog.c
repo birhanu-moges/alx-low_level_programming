ad66b71ab516bac8a52192ef03c423fdd64a8434
birhanu-moges <birhanu18@gmail.com>
2023-01-16 05:37:23 -0800

    task 3

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * print_dog - print dog object
+ * @d: pointer to dog object
+ *
+ *
+ *
+ * Return: void, and set dog object through pointer
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)\n"));
+		printf("Age: %f\n", d->age);
+		d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n"));
+	}
+}
