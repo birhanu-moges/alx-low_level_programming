65e656bc6ed7fe772df478085eafe1454f7b919d
birhanu-moges <birhanu18@gmail.com>
2023-01-16 10:54:01 -0800

    task 6

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - create and initilize dog object
+ * @name: dog name
+ * @age: dog age
+ * @owner: doge owner name
+ *
+ * Return: new dog object
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
