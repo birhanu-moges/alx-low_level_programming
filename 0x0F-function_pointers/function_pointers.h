89a9525459e8791541ec21f73a6be094dddc300f
birhanu-moges <birhanu18@gmail.com>
2023-01-18 11:07:45 -0800

    task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stddef.h>
 int _putchar(char c);
 
 void print_name(char *name, void (*f)(char *));
