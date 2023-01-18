fd53589c293ede58bb7afde3b77734eb3b1f2504
birhanu-moges <birhanu18@gmail.com>
2023-01-18 11:36:24 -0800

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - print array elements using pointer fuction
+ * @array: array with elements
+ * @size: array size
+ * @action: pointer function
+ *
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
