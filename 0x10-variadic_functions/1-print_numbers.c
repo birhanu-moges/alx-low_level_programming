215e53493926f4c3d1c5f99bfb902ca8ac489f54
birhanu-moges <birhanu18@gmail.com>
2023-01-19 10:54:37 -0800

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,14 +12,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list list;
 
-	if (n == 0)
-		return;
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(list, int));
 		if (i != (n - 1) && separator != NULL)
-                        printf("%c", *separator);
+			printf("%c", *separator);
 	}
 	printf("\n");
 	va_end(list);
