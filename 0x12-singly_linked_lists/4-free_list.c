47eceb1ff6fcb77614eedd316ae5fe411858572f
birhanu-moges <birhanu18@gmail.com>
2023-01-26 09:43:41 -0800

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_list - free memory used by linked lis
+ * @head: head pointer to list
+ *
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+	free_list(head->next);
+	free(head);
+}
