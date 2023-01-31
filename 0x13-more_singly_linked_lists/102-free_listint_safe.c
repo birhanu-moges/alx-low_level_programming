ab00bebcd27f1294a129eaf9ebd1b554ecf22a40
birhanu-moges <birhanu18@gmail.com>
2023-01-31 10:02:12 -0800

    task 13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - fre lists with loop
+ * @h: head pointer to list
+ *
+ * Return: return the size of a list
+ */
+
+size_t free_listint_safe(lintint_t **h)
+{
+	listint_t *sp, *fp, tmp;
+	unsigned int flag = 0, index = 0;
+
+	if (*h == NULL)
+		return (*h);
+	sp = fp = tmp = *h;
+	while (sp && fp && fp->next)
+	{
+		sp = sp->next;
+		fp = sp->next->next;
+		if (sp == fp)
+		{
+			flag = 1;
+			break;
+		}
+	}
+
+	if (flag)
+	{
+		while (tmp->next != sp->next)
+		{
+			tmp = tmp->next;
+			sp = sp->next;
+		}
+		sp->next = NULL;
+	}
+	tmp = *h;
+	while (tmp)
+	{
+		index++;
+		tmp = tmp->next;
+	}
+
+	return (index);
+}
