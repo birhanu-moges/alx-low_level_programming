dd150f29d34de1ad9f4a3fe8ac138f2bea6a9a3b
birhanu-moges <birhanu18@gmail.com>
2023-01-30 23:08:05 -0800

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,6 +39,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			}
 			pervious->next = newNode;
 			newNode->next = current;
+			return (newNode);
 		}
 		pervious = current;
 		current = current->next;
