a851d46f2cc713174f5458a11ed8cce4b1e6b07b
birhanu-moges <birhanu18@gmail.com>
2023-02-06 09:10:50 -0800

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,15 +17,14 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd == -1)
 		return (-1);
-	while (text_content[i] != '\0')
-		i++;
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
+		while (text_content[i])
+			i++;
 	}
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	wr = write(fd, text_content, i);
-	if (wr == -1)
+	if (wr == -1 || fd == -1)
 		return (-1);
 	close(fd);
 	return (1);
