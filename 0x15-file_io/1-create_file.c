0131cc261a7343b812cbc6cd8b6653e60680ddbf
birhanu-moges <birhanu18@gmail.com>
2023-02-06 09:18:43 -0800

    task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,9 +14,6 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
-	if (fd == -1)
-		return (-1);
 	if (text_content != NULL)
 	{
 		while (text_content[i])
