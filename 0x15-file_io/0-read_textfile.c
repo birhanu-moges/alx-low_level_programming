5419ef4f1eeb6f1ab00850d7ea4d9940d5a30e55
birhanu-moges <birhanu18@gmail.com>
2023-02-06 05:41:20 -0800

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints on POSIX standard output
+ * @filename: file name
+ * @letters: no of letters read workds on
+ *
+ * Return: no of letters read or (0) on failure
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, rd, wr;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, 0_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char)*letters +1);
+	if (buf == NULL)
+		return (0);
+	rd = read(fd, buf, letters);
+	if (rd == -1)
+		return (0);
+	buf[letters] = '\0';
+	wr = write(STDOUT_FILENO, buf, rd);
+	if (wr == -1)
+		return (0);
+	close(fd);
+	free(buf);
+	return (wr);
+}
