44a7259f2101566aa7a6d2133729e7b3582b405c
birahnu-moges <birhanu18@gmail.com>
2023-03-20 06:52:40 -0700

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,7 +12,7 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t count = 0;
 
 	current = h;
-	while (current)
+	while (current != NULL)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
