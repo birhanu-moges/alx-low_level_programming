31bb2e5ef5cb8465f66a5619300287e7446e2eaa
birahnu-moges <birhanu18@gmail.com>
2023-03-20 06:48:25 -0700

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_slistint - prints list elements
+ * @h: head of a list
+ * Return: number of node or 0
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current;
+	size_t count = 0;
+
+	current = h;
+	while(current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		count++;
+	}
+	return count;
+}
