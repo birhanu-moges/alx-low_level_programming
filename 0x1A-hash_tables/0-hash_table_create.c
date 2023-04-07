5282b336681fd6ad501cf0e54af649a0e2cd95d9
birahnu-moges <birhanu18@gmail.com>
2023-04-07 00:32:29 -0700

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,10 +8,18 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t ptr;
+	hash_table_t *ptr;
+	unsigned long int i;
 
-	prt = malloc(sizeof(hash_table_t));
+	ptr = malloc(sizeof(hash_table_t));
+	if (ptr == NULL)
+		return (NULL);
 	ptr->size = size;
-	ptr->array = calloc(ptr->size, sizeof(hash_node_t));
-	return (ptr)
+	ptr->array = malloc(sizeof(hash_node_t*)*size);
+	if (ptr->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ptr->array[i] = NULL;
+
+	return (ptr);
 }
